660bd9489852d025860db377369d223251eaacfe
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-19 21:18:33 -0400

    Events can be added to each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,33 @@ let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '
 $("#currentDay").text(`${output}`);
 
 
+setInterval(checkTime(), (1000 * 60) * 5);
+
+
+// save tasks
+
+var loadTasks = function() {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!tasks) {
+    tasks = {
+      toDo: [],
+      inProgress: [],
+      inReview: [],
+      done: []
+    };
+  }
+
+  // loop over object properties
+  $.each(tasks, function(list, arr) {
+    // then loop over sub-array
+    arr.forEach(function(task) {
+      createTask(task.text, task.date, list);
+    });
+  });
+};
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}; 
