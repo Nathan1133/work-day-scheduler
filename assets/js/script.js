d023d527dc5da018e1a4177c02757322fbfbd55c
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-20 13:51:00 -0400

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// to show current day
-// adapted from https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-ii-7af7d239a55d
 let d = new Date();
 
 let year = d.getFullYear();
@@ -11,65 +9,65 @@ let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '
 $("#currentDay").text(`${output}`);
 
 
-setInterval(checkTime(), (1000 * 60) * 5);
+var checkTime = function () {
 
+    //Get Current time
+    var currentTime = moment().format('H');
+    console.log(currentTime);
 
-// save tasks
+    //get all one-hour time blocks with class "textarea"
+    var timeBlockElements = $(".textarea");
 
-var loadTasks = function() {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
+    //loop through time block classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
 
-  // if nothing in localStorage, create a new object to track all task status arrays
-  if (!tasks) {
-    tasks = {
-      toDo: [],
-      inProgress: [],
-      inReview: [],
-      done: []
-    };
-  }
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
 
-  // loop over object properties
-  $.each(tasks, function(list, arr) {
-    // then loop over sub-array
-    arr.forEach(function(task) {
-      createTask(task.text, task.date, list);
-    });
-  });
-};
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
 
-var saveTasks = function() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}; 
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
 
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+}
 
-setInterval(checkTime(), (1000 * 60) * 5);
+// checkTime 
+setInterval(checkTime(), (1000 * 60) * 2);
 
+// Saving tasks
 
-// creating, loading and saving tasks persistently
-var events = {};
+const businessHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
 var loadEvents = function() {
-  events = JSON.parse(localStorage.getItem("events"));
+    $("#09").val(localStorage.getItem("hour-9"));
+    $("#10").val(localStorage.getItem("hour-10"));
+    $("#11").val(localStorage.getItem("hour-11"));
+    $("#12").val(localStorage.getItem("hour-12"));
+    $("#13").val(localStorage.getItem("hour-13"));
+    $("#14").val(localStorage.getItem("hour-14"));
+    $("#15").val(localStorage.getItem("hour-15"));
+    $("#16").val(localStorage.getItem("hour-16"));
+    $("#17").val(localStorage.getItem("hour-17"));
+}
 
-  if (!events) {
-    events = {};
-  }
-
-  $(".saveBtn").click(function() {
-    var eventText = $(".textarea").val();
-
-    // events.push({
-    //     text: eventText,
-    // });
-
-    saveEvents();
+for (i = 0; i < businessHours.length; i++) {
+    $(".saveBtn").click(function(event)  {
+        var textValue = $(this).siblings(".textarea").val();
+        var time = $(this).parent().attr("id")
+        console.log(time);
+    
+        localStorage.setItem(time, textValue);
     });
 };
 
-var saveEvents = function() {
-    localStorage.setItem("events", JSON.stringify(events));
-};
-
-saveEvents();
-loadEvents();
+loadEvents()
